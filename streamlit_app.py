a1a9676ae26ffe784d72ddb01bc08417cd2a8650
priyann012 <132575369+priyann012@users.noreply.github.com>
2023-05-04 18:25:22 +0530

    Update streamlit_app.py
    added brkfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy New Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Item 1')
+streamlit.title('Item 2')
+streamlit.title('Item 3')
